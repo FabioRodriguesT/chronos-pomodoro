90ec27824fe6fb68e1e8c68030ed61901b8ac0d9
Fabio Rodrigues <fabur13@gmail.com>
2026-09-14 15:09:18 -0300

    Create MainRouter and ScrolltoTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import Home from "./pages/Home";
-import AboutPomodoro from "./pages/AboutPomodoro";
-import NotFound from "./pages/NotFound";
-
 import TaskContextProvider from "./contexts/TaskContext/TaskContextProvider";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 import MessagesContainer from "./components/MessagesContainer";
-import { BrowserRouter, Routes, Route } from "react-router";
+import MainRouter from "./routers/MainRouter";
 
 function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-pomodoro" element={<AboutPomodoro />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
