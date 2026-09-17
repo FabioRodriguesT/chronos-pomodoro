cf1b2e17b5a5d54b54428119e160fbcd1d3e8200
Fabio Rodrigues <fabur13@gmail.com>
2026-09-17 09:32:21 -0300

    Create settings page.

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -4,6 +4,7 @@ import Home from "../../pages/Home";
 import NotFound from "../../pages/NotFound";
 import { useEffect } from "react";
 import History from "../../pages/History";
+import Settings from "../../pages/Settings";
 
 const ScrolltoTop = () => {
   const pathname = useLocation();
@@ -22,6 +23,7 @@ const MainRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about-pomodoro" element={<AboutPomodoro />} />
         <Route path="/history" element={<History />} />
+        <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ScrolltoTop />
